cba0c2258a45cd7670bdcae79a503abf87044e7c
David Yoon <davidyoon@hanwha.com>
2024-10-24 17:31:53 +0900

    Add new api
    * Added random, specific id and except id request

--- src/wod/wod.controller.ts
+++ src/wod/wod.controller.ts
@@ -1,18 +1,36 @@
-import { BadRequestException, Controller, Get, Query } from '@nestjs/common';
+import { BadRequestException, Controller, Get, Param, Query } from '@nestjs/common';
 import { WodService } from './wod.service';
+import { query } from 'express';
 
 @Controller('wod')
 export class WodController {
   constructor(private readonly wodService: WodService) {}
 
   @Get()
-  findProgram(@Query('methodType') method, @Query('level') level) {
+  getRandomProgram(@Query('methodType') method, @Query('level') level, @Query('id') id) {
     console.log(method, level);
 
     if (method == null || method == '' || level == null || level == '') {
       throw new BadRequestException('Invalid parameter');
     }
-
-    return this.wodService.findProgram(method, level);
+    
+    if (id == null || id == '') {
+      return this.wodService.findRandomProgram(method, level);
+    } else {
+      return this.wodService.findOtherProgram(method, level, id);
+    }
   }
+
+  @Get(':id')
+  findByid(@Param('id') id: string, @Query('methodType') method, @Query('level') level) {
+    if(!id) {
+      throw new BadRequestException('ID parameter is required');
+    }
+
+    console.log("find by id : ", id)
+
+    return this.wodService.findProgram(method, level, id)
+  }
+
+
 }
--- src/wod/wod.service.ts
+++ src/wod/wod.service.ts
@@ -27,67 +27,210 @@ import {
   machineIntermediateB,
   machineIntermediateC,
 } from './entities/mock/machineProgram.data';
+import { Program } from './entities/program.entity';
 
 @Injectable()
 export class WodService {
-  findProgram(method: string, level: string) {
-    return this.createProgram(method, level);
+  findRandomProgram(method: string, level: string) {
+    return this.getRandomProgram(method, level);
   }
 
-  createProgram(method: string, level: string) {
+  findOtherProgram(method: string, level: string, id: string) {
+    return this.getOtherProgramExceptId(method, level, id);
+  }
+
+  findProgram(method: string, level: string, id: string) {
+    return this.getProgramById(method, level, id);
+  }
+
+  getRandomProgram(method: string, level: string) {
     if (method == 'body' && level == 'beginner') {
-      const randomIndex = this.getRandomIndex();
       const dataArray = [bodyBegginerA, bodyBegginerB, bodyBegginerC];
+
+      const randomIndex = this.getRandomIndex(dataArray.length);
       return dataArray[randomIndex];
     } else if (method == 'body' && level == 'elementary') {
-      const randomIndex = this.getRandomIndex();
       const dataArray = [bodyElementaryA, bodyElementaryB, bodyElementaryC];
+
+      const randomIndex = this.getRandomIndex(dataArray.length);
       return dataArray[randomIndex];
     } else if (method == 'body' && level == 'intermediate') {
-      const randomIndex = this.getRandomIndex();
+      
       const dataArray = [
         bodyIntermediateA,
         bodyIntermediateB,
         bodyIntermediateC,
       ];
+      const randomIndex = this.getRandomIndex(dataArray.length);
+
       return dataArray[randomIndex];
     } else if (method == 'body' && level == 'advanced') {
-      const randomIndex = this.getRandomIndex();
       const dataArray = [bodyAdvancedA, bodyAdvancedB, bodyAdvancedC];
+
+      const randomIndex = this.getRandomIndex(dataArray.length);
       return dataArray[randomIndex];
     }
 
     if (method == 'machine' && level == 'beginner') {
-      const randomIndex = this.getRandomIndex();
       const dataArray = [machineBeginnerA, machineBeginnerB, machineBeginnerC];
+
+      const randomIndex = this.getRandomIndex(dataArray.length);
       return dataArray[randomIndex];
     } else if (method == 'machine' && level == 'elementary') {
-      const randomIndex = this.getRandomIndex();
       const dataArray = [
         machineElementaryA,
         machineElementaryB,
         machineElementaryC,
       ];
+
+      const randomIndex = this.getRandomIndex(dataArray.length);
       return dataArray[randomIndex];
     } else if (method == 'machine' && level == 'intermediate') {
-      const randomIndex = this.getRandomIndex();
       const dataArray = [
         machineIntermediateA,
         machineIntermediateB,
         machineIntermediateC,
       ];
+      const randomIndex = this.getRandomIndex(dataArray.length);
       return dataArray[randomIndex];
     } else if (method == 'machine' && level == 'advanced') {
-      const randomIndex = this.getRandomIndex();
       const dataArray = [machineAdvancedA, machineAdvancedB, machineAdvancedC];
+      const randomIndex = this.getRandomIndex(dataArray.length);
       return dataArray[randomIndex];
     } else {
       throw new BadRequestException('Invalid parameter');
     }
   }
 
-  getRandomIndex() {
-    const randomIndex = Math.floor(Math.random() * 3);
+  getOtherProgramExceptId(method: string, level: string, id: string) {
+    if (method == 'body' && level == 'beginner') {
+      const dataArray = [bodyBegginerA, bodyBegginerB, bodyBegginerC];
+      const filteredArray: Program[] = dataArray.filter((program) => program.id != id)
+
+      const randomIndex = this.getRandomIndex(filteredArray.length);
+
+      return filteredArray[randomIndex];
+    } else if (method == 'body' && level == 'elementary') {
+      const dataArray = [bodyElementaryA, bodyElementaryB, bodyElementaryC];
+      const filteredArray: Program[] = dataArray.filter((program) => program.id != id)
+
+      const randomIndex = this.getRandomIndex(filteredArray.length);
+      return filteredArray[randomIndex];
+    } else if (method == 'body' && level == 'intermediate') {
+      const dataArray = [
+        bodyIntermediateA,
+        bodyIntermediateB,
+        bodyIntermediateC,
+      ];
+      const filteredArray: Program[] = dataArray.filter((program) => program.id != id)
+      const randomIndex = this.getRandomIndex(filteredArray.length);
+      return filteredArray[randomIndex];
+    } else if (method == 'body' && level == 'advanced') {
+      const dataArray = [bodyAdvancedA, bodyAdvancedB, bodyAdvancedC];
+      const filteredArray: Program[] = dataArray.filter((program) => program.id != id)
+      const randomIndex = this.getRandomIndex(filteredArray.length);
+      return filteredArray[randomIndex];
+    }
+
+    if (method == 'machine' && level == 'beginner') {
+      const dataArray = [machineBeginnerA, machineBeginnerB, machineBeginnerC];
+      const filteredArray: Program[] = dataArray.filter((program) => program.id != id)
+      const randomIndex = this.getRandomIndex(filteredArray.length);
+      return filteredArray[randomIndex];
+    } else if (method == 'machine' && level == 'elementary') {
+      const dataArray = [
+        machineElementaryA,
+        machineElementaryB,
+        machineElementaryC,
+      ];
+      const filteredArray: Program[] = dataArray.filter((program) => program.id != id)
+
+      const randomIndex = this.getRandomIndex(dataArray.length);
+      return filteredArray[randomIndex];
+    } else if (method == 'machine' && level == 'intermediate') {
+      const dataArray = [
+        machineIntermediateA,
+        machineIntermediateB,
+        machineIntermediateC,
+      ];
+      const filteredArray: Program[] = dataArray.filter((program) => program.id != id)
+      const randomIndex = this.getRandomIndex(filteredArray.length);
+      return filteredArray[randomIndex];
+    } else if (method == 'machine' && level == 'advanced') {
+      
+      const dataArray = [machineAdvancedA, machineAdvancedB, machineAdvancedC];
+      const filteredArray: Program[] = dataArray.filter((program) => program.id != id)
+      const randomIndex = this.getRandomIndex(filteredArray.length);
+      
+      return filteredArray[randomIndex];
+    } else {
+      throw new BadRequestException('Invalid parameter');
+    }
+  }
+
+  getProgramById(method: string, level: string, id: string) {
+    if (method == 'body' && level == 'beginner') {
+      const dataArray = [bodyBegginerA, bodyBegginerB, bodyBegginerC];
+      const result = dataArray.find((program) => program.id == id)
+      
+      return result;
+    } else if (method == 'body' && level == 'elementary') {
+      const dataArray = [bodyElementaryA, bodyElementaryB, bodyElementaryC];
+      const result = dataArray.find((program) => program.id == id)
+      
+      return result;
+    } else if (method == 'body' && level == 'intermediate') {
+      const dataArray = [
+        bodyIntermediateA,
+        bodyIntermediateB,
+        bodyIntermediateC,
+      ];
+      const result = dataArray.find((program) => program.id == id)
+      
+      return result;
+    } else if (method == 'body' && level == 'advanced') {
+      const dataArray = [bodyAdvancedA, bodyAdvancedB, bodyAdvancedC];
+      const result = dataArray.find((program) => program.id == id)
+      
+      return result;
+    }
+
+    if (method == 'machine' && level == 'beginner') {
+      const dataArray = [machineBeginnerA, machineBeginnerB, machineBeginnerC];
+
+      const result = dataArray.find((program) => program.id == id)
+      
+      return result;
+    } else if (method == 'machine' && level == 'elementary') {
+      const dataArray = [
+        machineElementaryA,
+        machineElementaryB,
+        machineElementaryC,
+      ];
+      const result = dataArray.find((program) => program.id == id)
+      
+      return result;
+    } else if (method == 'machine' && level == 'intermediate') {
+      const dataArray = [
+        machineIntermediateA,
+        machineIntermediateB,
+        machineIntermediateC,
+      ];
+      const result = dataArray.find((program) => program.id == id)
+      
+      return result;
+    } else if (method == 'machine' && level == 'advanced') {
+      const dataArray = [machineAdvancedA, machineAdvancedB, machineAdvancedC];
+      const result = dataArray.find((program) => program.id == id)
+      
+      return result;
+    } else {
+      throw new BadRequestException('Invalid parameter');
+    }
+  }
+
+  getRandomIndex(count: number) {
+    const randomIndex = Math.floor(Math.random() * count);
     return randomIndex;
   }
 }
